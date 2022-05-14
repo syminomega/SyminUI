4ba284f0774da1aebb5cf8f005cb412b12a2638c
Symin <syminomega@outlook.com>
2022-05-14 16:19:20 +0800

    Update emboss effect
    优化浮雕效果，调整按钮边缘色彩，调整按钮光影效果。调整圆角阴影

---
+++ SyminUI/Convertor/CornerRadiusMultiply.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SyminUI.Convertor
+{
+    [ValueConversion(typeof(CornerRadius), typeof(CornerRadius))]
+    public class CornerRadiusMultiply : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var originRadius = (CornerRadius)value;
+            var paramStr = parameter as string;
+            if (string.IsNullOrEmpty(paramStr))
+            {
+                return originRadius;
+            }
+            var paramArray = paramStr.Split(',');
+            var r0 = originRadius.TopLeft * double.Parse(paramArray[0]);
+            var r1 = originRadius.TopRight * double.Parse(paramArray[1]);
+            var r2 = originRadius.BottomRight * double.Parse(paramArray[2]);
+            var r3 = originRadius.BottomLeft * double.Parse(paramArray[3]);
+
+            return new CornerRadius(r0, r1, r2, r3);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SyminUI/Convertor/HalfValue.cs
+++ SyminUI/Convertor/HalfValue.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
-namespace SyminUI.Convertor.Slot
+namespace SyminUI.Convertor
 {
     public class HalfValue : IValueConverter
     {

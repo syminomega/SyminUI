479e0b8d54a70e77dee2bd9754bb765912c44bcc
Symin <syminomega@outlook.com>
2022-05-28 20:22:02 +0800

    Add lighted surface control. add card control
    添加动态光照控件，添加使用动态光照的卡片

---
+++ SyminUI/Controls/CardBorder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SyminUI.Controls
+{
+    public class CardBorder : ContentControl
+    {
+        static CardBorder()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CardBorder),
+                new FrameworkPropertyMetadata(typeof(CardBorder)));
+        }
+
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius",
+                typeof(CornerRadius), typeof(CardBorder),
+                new PropertyMetadata(new CornerRadius(4)));
+
+
+
+        public double LightSize
+        {
+            get { return (double)GetValue(LightSizeProperty); }
+            set { SetValue(LightSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LightSize.
+        public static readonly DependencyProperty LightSizeProperty =
+            DependencyProperty.Register("LightSize", 
+                typeof(double), typeof(CardBorder),
+                new PropertyMetadata(200.0));
+
+
+
+    }
+}
--- SyminUI/Controls/Decorations/LightedSurface.cs
+++ SyminUI/Controls/Decorations/LightedSurface.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,15 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SyminUI.Effects;
 
 namespace SyminUI.Controls.Decorations
 {
-    public class LightedSurface : Control
+    public class LightedSurface : ContentControl
     {
         static LightedSurface()
         {
@@ -23,40 +26,133 @@ namespace SyminUI.Controls.Decorations
                 new FrameworkPropertyMetadata(typeof(LightedSurface)));
         }
 
+        private LightedSurfaceEffect? _effect;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            InitEffect();
+        }
+        private void InitEffect()
+        {
+            var contentBorder = GetTemplateChild("PART_lightBorder") as Border;
+            _effect = new LightedSurfaceEffect
+            {
+                UISize = new Point(ActualWidth, ActualHeight),
+                LightSize = LightSize,
+                LightColor = LightColorBrush.Color,
+                Intensity = LightIntensity,
+            };
+            contentBorder!.Effect = _effect;
+        }
+
         protected override void OnMouseMove(MouseEventArgs e)
         {
             base.OnMouseMove(e);
-            MousePosition = e.GetPosition(this);
-            DisplayText = $"x:{MousePosition.X} y:{MousePosition.Y}";
+            if (_effect != null)
+            {
+                _effect.MousePosition = e.GetPosition(this);
+            }
         }
 
-
-
-        public Point MousePosition
+        // 更新Effect中的UI尺寸
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
-            get { return (Point)GetValue(MousePositionProperty); }
-            set { SetValue(MousePositionProperty, value); }
+            base.OnRenderSizeChanged(sizeInfo);
+            if (_effect != null)
+            {
+                _effect.UISize = new Point(sizeInfo.NewSize.Width, sizeInfo.NewSize.Height);
+            }
         }
 
-        // Using a DependencyProperty as the backing store for MousePosition.
-        public static readonly DependencyProperty MousePositionProperty =
-            DependencyProperty.Register("MousePosition",
-                typeof(Point), typeof(LightedSurface),
-                new PropertyMetadata(new Point(0, 0)));
-
-
-
-        public string DisplayText
+        public SolidColorBrush LightColorBrush
         {
-            get { return (string)GetValue(DisplayTextProperty); }
-            set { SetValue(DisplayTextProperty, value); }
+            get { return (SolidColorBrush)GetValue(LightColorBrushProperty); }
+            set { SetValue(LightColorBrushProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for DisplayText.
-        public static readonly DependencyProperty DisplayTextProperty =
-            DependencyProperty.Register("DisplayText",
-                typeof(string), typeof(LightedSurface),
-                new PropertyMetadata(""));
+        // Using a DependencyProperty as the backing store for LightBrush.
+        public static readonly DependencyProperty LightColorBrushProperty =
+            DependencyProperty.Register("LightColorBrush",
+                typeof(SolidColorBrush), typeof(LightedSurface),
+                new PropertyMetadata(Brushes.White, LightColorChangedCallback));
+
+        private static void LightColorChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var lightedSurface = d as LightedSurface;
+            if (lightedSurface == null)
+            {
+                return;
+            }
+            if (lightedSurface._effect != null)
+            {
+                lightedSurface._effect.LightColor = ((SolidColorBrush)e.NewValue).Color;
+            }
+        }
+
+        public double LightIntensity
+        {
+            get { return (double)GetValue(LightIntensityProperty); }
+            set { SetValue(LightIntensityProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LightIntensity.
+        public static readonly DependencyProperty LightIntensityProperty =
+            DependencyProperty.Register("LightIntensity",
+                typeof(double), typeof(LightedSurface),
+                new PropertyMetadata(0.0, LightIntensityChangedCallback));
+
+        private static void LightIntensityChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var lightedSurface = d as LightedSurface;
+            if (lightedSurface == null)
+            {
+                return;
+            }
+            if (lightedSurface._effect != null)
+            {
+                lightedSurface._effect.Intensity = (double)e.NewValue;
+            }
+        }
+
+        public double LightSize
+        {
+            get { return (double)GetValue(LightSizeProperty); }
+            set { SetValue(LightSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LightSize.
+        public static readonly DependencyProperty LightSizeProperty =
+            DependencyProperty.Register("LightSize",
+                typeof(double), typeof(LightedSurface),
+                new PropertyMetadata(40.0, LightSizeChangedCallback));
+
+        private static void LightSizeChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var lightedSurface = d as LightedSurface;
+            if (lightedSurface == null)
+            {
+                return;
+            }
+            if (lightedSurface._effect != null)
+            {
+                lightedSurface._effect.LightSize = (double)e.NewValue;
+            }
+        }
+
+
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius",
+                typeof(CornerRadius), typeof(LightedSurface),
+                new PropertyMetadata(new CornerRadius(4)));
 
 
     }

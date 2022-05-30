2083362863a64d1210be01cc0a6a1aaf4ca219fa
Symin <syminomega@outlook.com>
2022-05-30 14:01:33 +0800

    Add Slider Style (Basic functions)
    初步添加 Slider 样式。
    调整 Textbox。

---
+++ SyminUI/Convertor/Slider/ValueToRangeWidth.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Controls;
+
+namespace SyminUI.Convertor.Slider
+{
+    public class ValueToRangeWidth : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var paramStr = parameter as string;
+            _ = double.TryParse(paramStr, out var margin);
+
+            var minimum = (double)values[0];
+            var maximum = (double)values[1];
+            var currentValue = (double)values[2];
+            var width = (double)values[3];
+            var actualWidth = width - 2 * margin;
+
+            if (actualWidth < 0)
+            {
+                actualWidth = 0;
+            }
+            if (currentValue < minimum)
+            {
+                return 0;
+            }
+            if (currentValue > maximum)
+            {
+                return actualWidth;
+            }
+
+            return ((currentValue - minimum) / (maximum - minimum)) * actualWidth;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

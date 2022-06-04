a10989b6cb57d60cbd8eaa4500ebb8d4fec10caf
Symin <syminomega@outlook.com>
2022-06-04 13:04:14 +0800

    Add more button styles, adjust resource storage
    添加更多按钮样式，修改资源存储方式，使用附加属性

---
+++ SyminUI/Controls/Attach/ShadingElement.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SyminUI.Controls.Attach
+{
+    public class ShadingElement
+    {
+
+        public static SolidColorBrush GetLightBrush(DependencyObject obj)
+        {
+            return (SolidColorBrush)obj.GetValue(LightBrushProperty);
+        }
+
+        public static void SetLightBrush(DependencyObject obj, SolidColorBrush value)
+        {
+            obj.SetValue(LightBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for LightBrush.
+        public static readonly DependencyProperty LightBrushProperty =
+            DependencyProperty.RegisterAttached("LightBrush",
+                typeof(SolidColorBrush), typeof(ShadingElement),
+                new PropertyMetadata(Brushes.White));
+
+
+
+
+        public static SolidColorBrush GetShadowBrush(DependencyObject obj)
+        {
+            return (SolidColorBrush)obj.GetValue(ShadowBrushProperty);
+        }
+
+        public static void SetShadowBrush(DependencyObject obj, SolidColorBrush value)
+        {
+            obj.SetValue(ShadowBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ShadowBrush. 
+        public static readonly DependencyProperty ShadowBrushProperty =
+            DependencyProperty.RegisterAttached("ShadowBrush",
+                typeof(SolidColorBrush), typeof(ShadingElement),
+                new PropertyMetadata(Brushes.Black));
+
+
+
+
+        public static Brush GetDisabledBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(DisabledBackgroundProperty);
+        }
+
+        public static void SetDisabledBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(DisabledBackgroundProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for DisabledBackground.
+        public static readonly DependencyProperty DisabledBackgroundProperty =
+            DependencyProperty.RegisterAttached("DisabledBackground",
+                typeof(Brush), typeof(ShadingElement),
+                new PropertyMetadata(Brushes.Gray));
+
+
+
+        public static Brush GetDisabledForeground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(DisabledForegroundProperty);
+        }
+
+        public static void SetDisabledForeground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(DisabledForegroundProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for DisabledForeground.
+        public static readonly DependencyProperty DisabledForegroundProperty =
+            DependencyProperty.RegisterAttached("DisabledForeground",
+                typeof(Brush), typeof(ShadingElement),
+                new PropertyMetadata(Brushes.GhostWhite));
+
+
+
+        public static Brush GetLightedBorder(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(LightedBorderProperty);
+        }
+
+        public static void SetLightedBorder(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(LightedBorderProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for LightedBorder.
+        public static readonly DependencyProperty LightedBorderProperty =
+            DependencyProperty.RegisterAttached("LightedBorder",
+                typeof(Brush), typeof(ShadingElement),
+                new PropertyMetadata(Brushes.White));
+
+
+    }
+}

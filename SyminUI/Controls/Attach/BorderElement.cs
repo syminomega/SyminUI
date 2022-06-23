07572f2a6bd98af7731ef7ad999922da173c14aa
Symin <syminomega@outlook.com>
2022-06-23 22:45:33 +0800

    Update flatten button style and menu style

---
+++ SyminUI/Controls/Attach/BorderElement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SyminUI.Controls.Attach
+{
+    public class BorderElement: DependencyObject
+    {
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius", 
+                typeof(CornerRadius), typeof(BorderElement),
+                new PropertyMetadata(new CornerRadius(4)));
+
+    }
+}
